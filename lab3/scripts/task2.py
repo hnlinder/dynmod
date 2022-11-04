b9025c15a4832f0e15fdba3668889c7a1587a437
hnlinder <henrik.linder.umu@gmail.com>
2022-11-04 20:37:00 +0100

    Fixed figs for lab3

--- lab3/scripts/task2.py
+++ lab3/scripts/task2.py
@@ -1,6 +1,8 @@
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("tkagg")
 import glob
 import scipy
 
@@ -44,7 +46,7 @@ def plot_degree_dist(udegrees, Pd, show=False, fig_nr=None, network_name=None,le
         plt.show()
 
 
-def plot_clustering_coefficient(clustering_coeff, bins=None, show=False, fig_nr=None, network_name=None, legend_on=True, normalized=False, line_type=None, see_through=True):
+def plot_clustering_coefficient(clustering_coeff, bins=None, show=False, fig_nr=None, network_name=None, legend_on=True, normalized=False, line_type=None, see_through=False):
     clustering_coeff = list(clustering_coeff)
     if fig_nr is not None:
         if fig_nr==0:
@@ -70,7 +72,7 @@ def plot_clustering_coefficient(clustering_coeff, bins=None, show=False, fig_nr=
     else:
         plt.yscale("log")
         if network_name is not None:
-            plt.hist(clustering_coeff,bins,ls=line_type, lw=3,label=network_name,edgecolor=color,fc=color,alpha=alpha)
+            plt.hist(clustering_coeff,bins,histtype="step",ls=line_type, lw=3,label=network_name,edgecolor=color,fc=color,alpha=alpha)
             # plt.hist(clustering_coeff,bins, ls="dashed", lw=3,label=network_name,fc="None")
         else:
             plt.hist(clustering_coeff,bins)
@@ -113,7 +115,7 @@ def get_shortest_path_lengths(G):
 
 
 
-def plot_spl_hist(spl_list, bins=None, show=False, fig_nr=None, network_name=None, legend_on=True, normalized=False, line_type=None, see_through=True):
+def plot_spl_hist(spl_list, bins=None, show=False, fig_nr=None, network_name=None, legend_on=True, normalized=False, line_type=None, see_through=False):
     if fig_nr is not None:
         if fig_nr==0:
             plt.figure()
@@ -137,7 +139,7 @@ def plot_spl_hist(spl_list, bins=None, show=False, fig_nr=None, network_name=Non
     else:
         plt.yscale("log")
         if network_name is not None:
-            plt.hist(spl_list,bins,ls=line_type, lw=3,label=network_name,edgecolor=color,fc=color,alpha=alpha)
+            plt.hist(spl_list,bins,histtype="step",ls=line_type, lw=3,label=network_name,edgecolor=color,fc=color,alpha=alpha)
         else:
             plt.hist(spl_list,bins)
     # if network_name is not None:
@@ -277,8 +279,8 @@ def mean_degree_dist(unique_degree_arr, degree_dist_arr):
 network_paths = ["../networks/yeast_gene_net.txt", "../networks/human_kidney_protein_net.txt", "../networks/ecoli_metabolic_net.txt" ]
 network_names = [ "Yeast gene network", "Human kidney protein", "E.Coli metabolic net"]
 
-save_plots =True
-draw_networks =False
+save_plots = True
+draw_networks = False
 nr_random_networks = 30
 if __name__=="__main__":
 
@@ -291,7 +293,8 @@ if __name__=="__main__":
         # network_names = 3*["Random Network"]
         # G = G_rand
         if draw_networks:
-            plt.figure(i+10)
+            print(f"At plot, i={i}, type={type(i)}")
+            plt.figure(int(i+10))
             nx.draw(G)
         print(G)
         print(network_names[i])
@@ -306,7 +309,7 @@ if __name__=="__main__":
         # if save_plots:
             # save_plot(network_names[i].replace(" ", "_").lower()+"_clustering_coeff_v","../report/figs/task2/")
         spl=get_shortest_path_lengths(G)
-        plot_spl_hist(spl,fig_nr=i+4, network_name=network_names[i])
+        plot_spl_hist(spl,fig_nr=i+4, network_name=network_names[i], normalized=False)
         # if save_plots:
             # save_plot(network_names[i].replace(" ", "_").lower()+"_spl_v","../report/figs/task2/")
         print(np.unique(spl))
@@ -333,8 +336,8 @@ if __name__=="__main__":
         degree_dist = []
         spl = []
         clustering_coeff = np.mean(clustering_coeff_arr, axis=0)
-        plt.figure(99)
-        plt.plot(clustering_coeff,label=network_names[i])
+        # plt.figure(99)
+        # plt.plot(clustering_coeff,label=network_names[i])
         # for k in range(nr_nodes):
             # clustering_coeff[k]= np.mean(clustering_coeff_arr[:,k])
             # unique_degrees = get_uniques(unique_degrees_arr)
@@ -355,10 +358,10 @@ if __name__=="__main__":
         # plot_degree_dist(unique_degrees,degree_dist,fig_nr=1 , network_name=f"{network_names[i]} random")
         # if save_plots:
             # save_plot(network_names[i].replace(" ", "_").lower()+"_degree_dist_v","../report/figs/task1/")
-        plot_clustering_coefficient(clustering_coeff, fig_nr=i+1, network_name=f"{network_names[i]} random")
+        plot_clustering_coefficient(clustering_coeff, fig_nr=i+1, network_name=f"{network_names[i]} random", line_type="--")
         if save_plots:
             save_plot(network_names[i].replace(" ", "_").lower()+f"_clustering_coeff_w_{nr_random_networks}_rand_v","../report/figs/task2/")
-        plot_spl_hist(spl,fig_nr=i+4, network_name=f"{network_names[i]} random",normalized=True)
+        plot_spl_hist(spl,fig_nr=i+4, network_name=f"{network_names[i]} random",normalized=False,line_type="--")
         if save_plots:
             save_plot(network_names[i].replace(" ", "_").lower()+f"_spl_w_{nr_random_networks}_rand_v","../report/figs/task2/")
 
