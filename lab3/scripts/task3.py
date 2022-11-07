9a7f92dca115c2bc2a0bb233edb750150ecbdc47
hnlinder <henrik.linder.umu@gmail.com>
2022-11-07 17:13:15 +0100

    Fixed saveplot

--- lab3/scripts/task3.py
+++ lab3/scripts/task3.py
@@ -194,28 +194,28 @@ def plot_hist(hist_list, bins=None,x_label=None, y_label=None, show=False, fig_n
 def save_plot(filename, path="",extension=".eps", fig_nr=None, increment_filename=True):
     if fig_nr is not None:
         plt.figure(fig_nr)
+    new_num = ""
     if increment_filename:
         num = []
-        new_num = "0"
         matching_files = glob.glob(f"{path}{filename}*")
-        print(matching_files)
+        # print(matching_files)
         if (len(matching_files) > 0):
             max_num = -1
             for file in matching_files:
                 fname = file.replace(path,"").split(".")[-2]
-                digits = ''.join(x for x in fname[-3:] if x.isdigit())
-                try:
+                digits = ''.join(x for x in fname[-3:] if x.isdigit()) #crashes if len(fname)<3
+                try: #I reeeeaaally really
                     num.append(int(digits))
-                except KeyboardInterrupt:
+                except KeyboardInterrupt: #Fucking hate 
                     pass
-                except:
+                except: # This Try-Except
                     pass
             max_num = max(num)
             if ((max_num)>(-1)):
                 new_num = str((max_num)+1)
             else:
                 new_num = "0"
-        plt.savefig(f"{path}{filename}{new_num}{extension}")
+    plt.savefig(f"{path}{filename}{new_num}{extension}")
 
 
 def test_plot_increment():
@@ -274,7 +274,7 @@ network_names = [ "Yeast gene network", "Human kidney protein", "E.Coli metaboli
 
 
 
-save_plots = False
+save_plots = True
 draw_networks =False
 nr_runs_per_f = 20
 nr_fs = 6
